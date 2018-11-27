a2ce94cd7597f94a7e97fcedb6906d65bc55cf23
Teofix <matteoferro93@hotmail.it>
2018-11-27 12:55:23 +0100

    Breci

--- app/src/main/java/com/adev/android/legomindfuck/MotorActivity.java
+++ app/src/main/java/com/adev/android/legomindfuck/MotorActivity.java
@@ -1,13 +1,17 @@
 package com.adev.android.legomindfuck;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
-import android.widget.SeekBar;
 import android.widget.Switch;
+
+import com.marcinmoskala.arcseekbar.ArcSeekBar;
+import com.marcinmoskala.arcseekbar.ProgressListener;
 
 public class MotorActivity extends AppCompatActivity {
 
@@ -17,15 +21,19 @@ public class MotorActivity extends AppCompatActivity {
 
     private ImageButton mButtonBaseLeft;
     private ImageButton mButtonBaseRight;
-    private SeekBar mSeekBarBase;
+    private ArcSeekBar mSeekBarBase;
 
     private ImageButton mButtonBraccioLeft;
     private ImageButton mButtonBraccioRight;
-    private SeekBar mSeekBarBraccio;
+    private ArcSeekBar mSeekBarBraccio;
 
     private ImageButton mButtonManoLeft;
     private ImageButton mButtonManoRight;
-    private SeekBar mSeekBarMano;
+    private ArcSeekBar mSeekBarMano;
+
+    private int mBaseDegree = 180;
+    private int mBraccioDegree = 180;
+    private int mManoDegree = 180;
 
     private Switch mSpeedSwitch;
 
@@ -37,6 +45,8 @@ public class MotorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motor);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         ev3 = new SocketManager();
         ev3.openSocket();
@@ -57,7 +67,9 @@ public class MotorActivity extends AppCompatActivity {
         mButtonBaseLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ev3.sendMessage(mMotorBase.move(2, 50, "+"));
+                ev3.sendMessage(mMotorBase.move(2, mSpeed, "-"));
+                mBaseDegree = ((mBaseDegree * 100 / 360) - 2) * 360 / 100;
+                mSeekBarBase.setProgress(mBaseDegree * 100 / 360);
             }
         });
 
@@ -65,44 +77,33 @@ public class MotorActivity extends AppCompatActivity {
         mButtonBaseRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ev3.sendMessage(mMotorBase.move(2, 50, "-"));
+                ev3.sendMessage(mMotorBase.move(2, mSpeed, "+"));
+                mBaseDegree = ((mBaseDegree * 100 / 360) + 2) * 360 / 100;
+                mSeekBarBase.setProgress(mBaseDegree * 100 / 360);
             }
         });
 
-        mSeekBarBase = (SeekBar) findViewById(R.id.seekBar1);
-        mSeekBarBase.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int oldDegrees = 0;
-            int newDegrees = 0;
-
+        mSeekBarBase = (ArcSeekBar) findViewById(R.id.seekBar1);
+        mSeekBarBase.setOnStopTrackingTouch(new ProgressListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                newDegrees = 360 * progress / 100;
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                //Log.i("Gradi", String.valueOf(newDegrees));
-
-                if (oldDegrees - newDegrees < 0) {
-                    ev3.sendMessage(mMotorBase.move(newDegrees - oldDegrees, mSpeed, "+"));
-                } else if (oldDegrees - newDegrees > 0) {
-                    ev3.sendMessage(mMotorBase.move(oldDegrees - newDegrees, mSpeed, "-"));
+            public void invoke(int i) {
+                if (i * 360 / 100 > mBaseDegree) {
+                    ev3.sendMessage(mMotorBase.move(i * 360 / 100 - mBaseDegree, mSpeed, "+"));
+                } else if (i * 360 / 100 < mBaseDegree) {
+                    ev3.sendMessage(mMotorBase.move(mBaseDegree - i * 360 / 100, mSpeed, "-"));
                 }
-
-                oldDegrees = newDegrees;
+                mBaseDegree = i * 360 / 100;
             }
         });
+        mSeekBarBase.setProgress(50);
 
         mButtonBraccioLeft = (ImageButton) findViewById(R.id.motor2_left);
         mButtonBraccioLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ev3.sendMessage(mMotorBraccio.move(2, 50, "+"));
+                ev3.sendMessage(mMotorBraccio.move(2, mSpeed, "-"));
+                mBraccioDegree = ((mBraccioDegree * 100 / 360) - 2) * 360 / 100;
+                mSeekBarBraccio.setProgress(mBraccioDegree * 100 / 360);
             }
         });
 
@@ -110,44 +111,33 @@ public class MotorActivity extends AppCompatActivity {
         mButtonBraccioRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ev3.sendMessage(mMotorBraccio.move(2, 50, "-"));
+                ev3.sendMessage(mMotorBraccio.move(2, mSpeed, "+"));
+                mBraccioDegree = ((mBraccioDegree * 100 / 360) + 2) * 360 / 100;
+                mSeekBarBraccio.setProgress(mBraccioDegree * 100 / 360);
             }
         });
 
-        mSeekBarBraccio = (SeekBar) findViewById(R.id.seekBar2);
-        mSeekBarBraccio.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int oldDegrees = 0;
-            int newDegrees = 0;
-
+        mSeekBarBraccio = (ArcSeekBar) findViewById(R.id.seekBar2);
+        mSeekBarBraccio.setOnStopTrackingTouch(new ProgressListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                newDegrees = 360 * progress / 100;
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                //Log.i("Gradi", String.valueOf(newDegrees));
-
-                if (oldDegrees - newDegrees < 0) {
-                    ev3.sendMessage(mMotorBraccio.move(newDegrees - oldDegrees, mSpeed, "+"));
-                } else if (oldDegrees - newDegrees > 0) {
-                    ev3.sendMessage(mMotorBraccio.move(oldDegrees - newDegrees, mSpeed, "-"));
+            public void invoke(int i) {
+                if (i * 360 / 100 > mBaseDegree) {
+                    ev3.sendMessage(mMotorBraccio.move(i * 360 / 100 - mBraccioDegree, mSpeed, "+"));
+                } else if (i * 360 / 100 < mBaseDegree) {
+                    ev3.sendMessage(mMotorBraccio.move(mBraccioDegree - i * 360 / 100, mSpeed, "-"));
                 }
-
-                oldDegrees = newDegrees;
+                mBraccioDegree = i * 360 / 100;
             }
         });
+        mSeekBarBraccio.setProgress(50);
 
         mButtonManoLeft = (ImageButton) findViewById(R.id.motor3_left);
         mButtonManoLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ev3.sendMessage(mMotorMano.move(2, 50, "+"));
+                ev3.sendMessage(mMotorMano.move(2, mSpeed, "-"));
+                mManoDegree = ((mManoDegree * 100 / 360) - 2) * 360 / 100;
+                mSeekBarMano.setProgress(mManoDegree * 100 / 360);
             }
         });
 
@@ -155,38 +145,25 @@ public class MotorActivity extends AppCompatActivity {
         mButtonManoRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ev3.sendMessage(mMotorMano.move(2, 50, "-"));
+                ev3.sendMessage(mMotorMano.move(2, mSpeed, "+"));
+                mManoDegree = ((mManoDegree * 100 / 360) + 2) * 360 / 100;
+                mSeekBarMano.setProgress(mManoDegree * 100 / 360);
             }
         });
 
-        mSeekBarMano = (SeekBar) findViewById(R.id.seekBar3);
-        mSeekBarMano.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int oldDegrees = 0;
-            int newDegrees = 0;
-
+        mSeekBarMano = (ArcSeekBar) findViewById(R.id.seekBar3);
+        mSeekBarMano.setOnStopTrackingTouch(new ProgressListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                newDegrees = 360 * progress / 100;
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                //Log.i("Gradi", String.valueOf(newDegrees));
-
-                if (oldDegrees - newDegrees < 0) {
-                    ev3.sendMessage(mMotorMano.move(newDegrees - oldDegrees, mSpeed, "+"));
-                } else if (oldDegrees - newDegrees > 0) {
-                    ev3.sendMessage(mMotorMano.move(oldDegrees - newDegrees, mSpeed, "-"));
+            public void invoke(int i) {
+                if (i * 360 / 100 > mManoDegree) {
+                    ev3.sendMessage(mMotorMano.move(i * 360 / 100 - mManoDegree, mSpeed, "+"));
+                } else if (i * 360 / 100 < mManoDegree) {
+                    ev3.sendMessage(mMotorMano.move(mManoDegree - i * 360 / 100, mSpeed, "-"));
                 }
-
-                oldDegrees = newDegrees;
+                mManoDegree = i * 360 / 100;
             }
         });
+        mSeekBarMano.setProgress(50);
 
         mSpeedSwitch = (Switch) findViewById(R.id.speed_switch);
         mSpeedSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
