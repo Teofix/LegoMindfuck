2b59f15ee7ec25a02b9622f2cda0179acc42adaa
theRealPier <43819479+theRealPier@users.noreply.github.com>
2018-12-08 10:26:16 +0100

    aaaa

--- app/src/main/java/com/adev/android/legomindfuck/Activity/ConnectionTestActivity.java
+++ app/src/main/java/com/adev/android/legomindfuck/Activity/ConnectionTestActivity.java
@@ -1,7 +1,11 @@
 package com.adev.android.legomindfuck.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +18,7 @@ import com.adev.android.legomindfuck.Thread.SocketManager;
 
 public class ConnectionTestActivity extends AppCompatActivity {
 
-    public static boolean isConnected = false;
+    public static boolean isConnected = true;
     private EditText ip;
     private EditText ip2;
     private TextView guide;
@@ -63,5 +67,30 @@ public class ConnectionTestActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu); //your file name
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.menItem_connection  :
+                break;
+
+            case R.id.menItem_play   :
+                Intent i = new Intent(getApplicationContext(), PlayMenuActivity.class);
+                startActivity(i);
+        }
+
+        return false;
     }
 }
--- app/src/main/java/com/adev/android/legomindfuck/Activity/MainMenuActivity.java
+++ app/src/main/java/com/adev/android/legomindfuck/Activity/MainMenuActivity.java
@@ -1,10 +1,17 @@
 package com.adev.android.legomindfuck.Activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.adev.android.legomindfuck.R;
 import com.adev.android.legomindfuck.ShowConnectionErrorMessage;
@@ -16,38 +23,121 @@ public class MainMenuActivity extends AppCompatActivity {
     private Button instr;
     private Button connection;
 
+    private TextView tHelp;
+    private ConstraintLayout mLayout;
 
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        SharedPreferences sharedPref = getSharedPreferences("access", MODE_PRIVATE);
+        int firstAccess = sharedPref.getInt("access", 0);
+
         play = findViewById(R.id.play_mode);
         instr = findViewById(R.id.mount_instructions);
         connection = findViewById(R.id.connection);
+        tHelp = findViewById(R.id.t_main_txt);
 
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(ConnectionTestActivity.isConnected)    {
-                    Intent i = new Intent(getApplicationContext(), PlayMenuActivity.class);
+        sharedPref.edit().putInt("access", 0).commit();
+
+        if(firstAccess==0) {
+
+            sharedPref.edit().putInt("access", 1).commit();
+
+            final ConstraintLayout mLayout = (ConstraintLayout) findViewById(R.id.mainMenuLayout);
+            mLayout.setBackgroundColor(R.color.mGrey);
+            instr.setAlpha((float) 0.4);
+            connection.setAlpha((float) 0.4);
+            final int[] taps = {0};
+
+
+            mLayout.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+
+                    switch(taps[0])    {
+                        case 0  :
+                            play.setAlpha((float) 0.4);
+                            instr.setAlpha(1);
+                            tHelp.setText("Accesso alle istruzioni di montaggio (tap per continuare)");
+                            taps[0]++;
+                            break;
+
+                        case 1  :
+                            instr.setAlpha((float) 0.4);
+                            connection.setAlpha(1);
+                            tHelp.setText("Accesso all'area per la connessione (tap per continuare)");
+                            taps[0]++;
+                            break;
+
+                        case 2:
+                            connection.setAlpha((float) 0.4);
+                            tHelp.setText("Premi play per accedere all'area gioco!");
+                            play.setAlpha(1);
+                            play.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent i = new Intent(getApplicationContext(), PlayMenuActivity.class);
+                                    startActivity(i);
+                                }
+                            });
+                    }
+                    return false;
+                }
+            });
+
+
+
+        }
+
+        else if(firstAccess==2) {
+
+            play.setAlpha((float) 0.4);
+            instr.setAlpha((float) 0.4);
+            tHelp.setText("Premi connect per accedere all'area di connessione");
+            final ConstraintLayout mLayout = (ConstraintLayout) findViewById(R.id.mainMenuLayout);
+            mLayout.setBackgroundColor(R.color.mGrey);
+
+            connection.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getApplicationContext(), ConnectionTestActivity.class);
                     startActivity(i);
                 }
-                else    {
-                    ShowConnectionErrorMessage err = new ShowConnectionErrorMessage();
-                    err.setContent("Errore di connessione!", "Robot non connesso all'app");
-                    err.show(getSupportFragmentManager(), "");
-                }
-            }
-        });
+            });
 
-        connection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), ConnectionTestActivity.class);
-                startActivity(i);
-            }
-        });
+        }
+
+        else {
+
+
+            play = findViewById(R.id.play_mode);
+            instr = findViewById(R.id.mount_instructions);
+            connection = findViewById(R.id.connection);
+
+            play.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (ConnectionTestActivity.isConnected) {
+                        Intent i = new Intent(getApplicationContext(), PlayMenuActivity.class);
+                        startActivity(i);
+                    } else {
+                        ShowConnectionErrorMessage err = new ShowConnectionErrorMessage();
+                        err.setContent("Errore di connessione!", "Robot non connesso all'app");
+                        err.show(getSupportFragmentManager(), "");
+                    }
+                }
+            });
+
+            connection.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getApplicationContext(), ConnectionTestActivity.class);
+                    startActivity(i);
+                }
+            });
 /*
         instr.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,5 +147,6 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
         */
+        }
     }
 }
