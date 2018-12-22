7881cb3b28c31d677751b713f1153af1d1e6b998
BUBERTO97 <roberto.amadio97@gmail.com>
2018-12-22 20:48:34 +0100

    aggiunto ad android manifest la parte dell'intent per endgame

--- app/src/main/java/com/adev/android/legomindfuck/Activity/PageViewerEndGame.java
+++ app/src/main/java/com/adev/android/legomindfuck/Activity/PageViewerEndGame.java
@@ -14,15 +14,15 @@ import com.adev.android.legomindfuck.R;
 
 public class PageViewerEndGame extends PagerAdapter {
 
-    Context context;
-    String[] title;
-    Integer[] manoDX;
-    Integer[] manoSX;
-    Integer[] braccioDX;
-    Integer[] braccioSX;
-    Integer[] baseDX;
-    Integer[] baseSX;
-    LayoutInflater inflater;
+    private Context context;
+    private String[] title;
+    private Integer[] manoDX;
+    private Integer[] manoSX;
+    private Integer[] braccioDX;
+    private Integer[] braccioSX;
+    private Integer[] baseDX;
+    private Integer[] baseSX;
+    private LayoutInflater inflater;
 
     private TextView t;
     private TextView tmanoDX;
@@ -45,29 +45,28 @@ public class PageViewerEndGame extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return title.length;
     }
 
     @Override
-    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+    public boolean isViewFromObject(View view, Object o) {
         return view==o;
     }
 
-    @NonNull
     @Override
-    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+    public Object instantiateItem(ViewGroup container, int position) {
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View itemView = inflater.inflate(R.layout.page_endgame, container, false);
+        final View itemView = inflater.inflate(R.layout.page_endgame, container, false);
 
-        t= itemView.findViewById(R.id.textView7);
-        tmanoDX= itemView.findViewById(R.id.clickmanodx);
-        tmanoSX= itemView.findViewById(R.id.clickmanosx);
+        t= (TextView) itemView.findViewById(R.id.textView7);
+        tmanoDX= (TextView) itemView.findViewById(R.id.clickmanodx);
+        tmanoSX= (TextView) itemView.findViewById(R.id.clickmanosx);
 
-        tbraccioDX= itemView.findViewById(R.id.clickbracciodx);
-        tbraccioSX= itemView.findViewById(R.id.clickbracciosx);
+        tbraccioDX= (TextView) itemView.findViewById(R.id.clickbracciodx);
+        tbraccioSX= (TextView) itemView.findViewById(R.id.clickbracciosx);
 
-        tbaseDX= itemView.findViewById(R.id.clickbasedx);
-        tbaseSX= itemView.findViewById(R.id.clickbasesx);
+        tbaseDX= (TextView) itemView.findViewById(R.id.clickbasedx);
+        tbaseSX= (TextView) itemView.findViewById(R.id.clickbasesx);
 
 
         ((ViewPager) container).addView(itemView);
