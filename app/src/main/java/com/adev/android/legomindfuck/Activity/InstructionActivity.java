3b4efec90a81c8351168220e410801426229d388
Alexander <861289@stud.unive.it>
2018-12-22 23:50:44 +0100

    done

--- app/src/main/java/com/adev/android/legomindfuck/Activity/InstructionActivity.java
+++ app/src/main/java/com/adev/android/legomindfuck/Activity/InstructionActivity.java
@@ -6,22 +6,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
 
 import com.adev.android.legomindfuck.R;
 
 public class InstructionActivity extends AppCompatActivity {
+
+    Button software;
+    Button hardware;
 
     WebView view;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_instruction);
         view = (WebView) findViewById(R.id.webview);
-        WebSettings webSettings = view.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        view.loadUrl("file:///android_asset/HowTo.html");
-        view.setVisibility(View.VISIBLE);
+        view.setVisibility(View.INVISIBLE);
+        software = (Button) findViewById(R.id.bSoftware);
+        hardware = (Button) findViewById(R.id.bHardware);
+
+        software.setOnClickListener(view1 -> {
+            software.setVisibility(View.INVISIBLE);
+            hardware.setVisibility(View.INVISIBLE);
+            WebSettings webSettings = view.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            view.loadUrl("file:///android_asset/HowTo.html");
+            view.setVisibility(View.VISIBLE);
+        } );
+
+        hardware.setOnClickListener(view2 -> {
+            software.setVisibility(View.INVISIBLE);
+            hardware.setVisibility(View.INVISIBLE);
+            WebSettings webSettings = view.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            //TODO: sostituire con istruzioni di montaggio
+            view.loadUrl("file:///android_asset/HowTo.html");
+            view.setVisibility(View.VISIBLE);
+        } );
 
     }
 
