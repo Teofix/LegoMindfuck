976727483497ae4f8ec81d6db17c746161345a37
Alexander <861289@stud.unive.it>
2019-01-20 16:44:36 +0100

    cleanUp

--- app/src/androidTest/java/com/adev/android/legomindfuQ/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/adev/android/legomindfuQ/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.adev.android.legomindfuck", appContext.getPackageName());
+        assertEquals("com.adev.android.legomindfuQ", appContext.getPackageName());
     }
 }
